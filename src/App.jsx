7513b08f49ad7788e7187f8784eede25d6784ef4
Martín Frangul <77142482+martinfrangul@users.noreply.github.com>
2024-10-07 16:31:52 +0200

    Agregado el estado de Loading y mejorado la limpieza de dato y errores

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,23 @@ function App() {
   const [filters, setFilters] = useState({});
   const [error, setError] = useState(null);
   const [filteredData, setFilteredData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const onFiltersChange = (newFilters) => {
     setFilters(newFilters);
   };
 
   const onGenerateHandler = async () => {
-    
+    setError(null);
+    setLoading(true);
+    setFilteredData([]);
 
     // SI NO HAY NINGÚN FILTRO ACTIVADO O ESTÁN TODOS LOS FILTROS ACTIVADOS LLAMAR AL ENDPOINT RANDOM
 
-    if (Object.values(filters).every((isActive) => !isActive) || Object.values(filters).every((isActive) => isActive)) {
+    if (
+      Object.values(filters).every((isActive) => !isActive) ||
+      Object.values(filters).every((isActive) => isActive)
+    ) {
       try {
         const response = await fetch("/api/random");
         if (!response.ok)
@@ -26,48 +32,53 @@ function App() {
         return;
       } catch (error) {
         setError(error.message);
-        return;
+      } finally {
+        setLoading(false);
       }
+      return;
     }
 
-    // SI HAY FILTROS ACTIVADOS 
+    // SI HAY FILTROS ACTIVADOS
 
     try {
-      const activeFilters = Object.keys(filters).filter((filter) => filters[filter]);
+      const activeFilters = Object.keys(filters).filter(
+        (filter) => filters[filter]
+      );
       const responses = await Promise.all(
         activeFilters.map((filter) =>
-          fetch(`/api/filter?type=${filter}`)
-            .then((response) => {
-              if (!response.ok) {
-                throw new Error("Error en la solicitud: " + response.statusText);
-              }
-              return response.json();
-            })
+          fetch(`/api/filter?type=${filter}`).then((response) => {
+            if (!response.ok) {
+              throw new Error("Error en la solicitud: " + response.statusText);
+            }
+            return response.json();
+          })
         )
       );
-    
+
       const combinedData = responses.flat();
-    
+
       // GENERAR UNA ACIVIDAD RANDOM DEL CONJUNTO DE ACTIVIDADES SELECCIONADAS POR LOS FILTROS
 
       setFilteredData((prev) => {
         if (combinedData.length > 0) {
-          const randomizedData = combinedData[Math.floor(Math.random() * combinedData.length)];
+          const randomizedData =
+            combinedData[Math.floor(Math.random() * combinedData.length)];
           return [randomizedData];
         }
         return prev;
       });
     } catch (error) {
-      console.error("Error al generar actividades:", error);
+      setError(error.message);
+    } finally {
+      setLoading(false);
     }
-    
   };
 
   return (
     <div className="max-w-96 lg:max-w-screen-lg flex flex-col m-auto justify-start items-center gap-3 h-screen lg:flex-row lg:">
-          <div className="flex w-full h-fit lg:w-1/5 justify-start lg:items-start lg:h-full">
-            <img src="/logo.png" className="w-32 h-fit p-3" alt="logo-image" />
-          </div>
+      <div className="flex w-full h-fit lg:w-1/5 justify-start lg:items-start lg:h-full">
+        <img src="/logo.png" className="w-32 h-fit p-3" alt="logo-image" />
+      </div>
       <div className="flex flex-col lg:flex-row justify-center gap-4 lg:items-center w-full h-5/6">
         <div className="flex flex-col w-full max-h-fit gap-4 lg:h-full items-center lg:w-3/5 lg:justify-center lg:border-r-2 lg:border-dashed lg:border-black">
           <Filters onFiltersChange={onFiltersChange} />
@@ -103,11 +114,13 @@ function App() {
             </div>
             <div>
               <h3 className="text-md text-center min-h-20 px-5 pb-5">
-                {error
-                  ? error 
-                  : filteredData.length > 0
-                    ? filteredData[0].activity 
-                    : "No s'han trobat activitats"}
+                {loading
+                  ? "Cargando..."
+                  : error
+                    ? error
+                    : filteredData.length > 0
+                      ? filteredData[0].activity
+                      : "No s'han trobat activitats"}
               </h3>
             </div>
           </div>
